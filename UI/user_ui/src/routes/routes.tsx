ddb23ff4bf528a186c698962afeb3680f37753c9
Carlos Valerio (Manuel) <carlos.valerio@dataart.com>
2025-10-03 09:35:44 -0600

    Adds sample report route

--- UI/user_ui/src/routes/routes.tsx
+++ UI/user_ui/src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Landing from '../Landing';
 import News from '../News';
+import SampleReport from '../SampleReport';
 
 
 export const router = createBrowserRouter([
@@ -11,5 +12,9 @@ export const router = createBrowserRouter([
   {
     path: '/news',
     element: <News />,
+  },
+  {
+    path: '/report-sample',
+    element: <SampleReport />,
   }
 ]);
