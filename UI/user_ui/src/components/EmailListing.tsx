2e5910b0a91035b2997ed0a2c1b00766a9a7de2a
Carlos Valerio (Manuel) <carlos.valerio@dataart.com>
2025-10-07 15:30:23 -0600

    Adds admin dashboard route

---
+++ UI/user_ui/src/components/EmailListing.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EmailListing() {
+  return (
+    <div>EmailListing</div>
+  )
+}
+
+export default EmailListing
---
+++ UI/user_ui/src/components/RegisterAdmin.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RegisterAdmin() {
+  return (
+    <div>RegisterAdmin</div>
+  )
+}
+
+export default RegisterAdmin
