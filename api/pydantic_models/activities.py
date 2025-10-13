937fda41414ca1a9649612a661dabebd85de4918
Carlos Valerio (Manuel) <carlos.valerio@dataart.com>
2025-10-13 17:21:48 -0600

    Update Config class to Configdict

--- api/pydantic_models/activities.py
+++ api/pydantic_models/activities.py
@@ -1,18 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import List, Optional
 from datetime import datetime
 
 
 class Activity(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int
     activity_type: str
     description: str
     occurred_at: Optional[datetime]
     entity_id: Optional[int]
     entity_type: str
-
-    class Config:
-        from_attributes = True
 
 
 class ActivitiesResponse(BaseModel):
