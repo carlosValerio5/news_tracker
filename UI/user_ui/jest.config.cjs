4f2f403596402fac8ac2e74f10ec23a00a445092
Carlos Valerio (Manuel) <carlos.valerio@dataart.com>
2025-10-22 17:38:06 -0600

    Adds formatting

--- UI/user_ui/jest.config.cjs
+++ UI/user_ui/jest.config.cjs
@@ -4,10 +4,10 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|svg|webp|ico|bmp|tiff)$": "jest-transform-stub",
-    "\\.(ttf|eot|woff|woff2|otf)$": "jest-transform-stub", 
+    "\\.(ttf|eot|woff|woff2|otf)$": "jest-transform-stub",
     "\\.(mp4|webm|wav|mp3|m4a|aac|oga)$": "jest-transform-stub",
     "^@/(.*)$": "<rootDir>/src/$1",
-    "^/src/(.*)$": "<rootDir>/src/$1", 
+    "^/src/(.*)$": "<rootDir>/src/$1",
   },
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   transform: {
